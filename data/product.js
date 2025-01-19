59f932e8b95b12a55bf6b5cf6f2e66bbb2231f10
Noel <noel.bordaje25@gmail.com>
2025-01-20 07:30:43 +0800

    use product.js file

---
+++ data/product.js
@@ -0,0 +1,149 @@
+const product = [{
+    image: 'images/6-piece-non-stick-baking-set.webp',
+    name: '6-Piece Nonstick, Carbon Steel Oven Bakeware',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2067,
+    
+}, {
+    image: 'images/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2095
+},{
+    image: 'images/plain-hooded-fleece-sweatshirt-yellow.jpg',
+    name: 'Plain Hooded Fleece Sweatshirt',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2899
+}, {
+    image: 'images/6-piece-white-dinner-plate-set.jpg',
+    name: '6 Piece White Dinner Plate Set',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2067
+},{
+    image: 'images/bathroom-rug.jpg',
+    name: 'Bathroom Bath Rug Mat 20 x 31 Inch - Grey',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 1250
+},{
+    image: 'images/blackout-curtain-set-beige.webp',
+    name: 'Blackout Curtains Set 4-Pack - Beige',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 4599
+},{
+    image: 'images/electric-glass-and-steel-hot-water-kettle.webp',
+    name: 'electric-glass-and-steel-hot-water-kettle',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 3074
+},{
+    image: 'images/facial-tissue-2-ply-18-boxes.jpg',
+    name: 'Ultra Soft Tissue 2-Ply - 18 Box',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 1050
+},{
+    image: 'images/knit-athletic-sneakers-gray.jpg',
+    name: 'athletic sneakers gray',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 5023
+
+},{
+    image: 'images/liquid-laundry-detergent-plain.jpg',
+    name: 'Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2899,
+    
+
+},{
+    image: 'images/luxury-tower-set-6-piece.jpg',
+    name: 'Luxury Towel Set - Graphite Gray',
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 3599,
+},{
+    image: 'images/men-golf-polo-t-shirt-blue.jpg',
+    name: `Men's Regular-Fit Quick-Dry Golf Polo Shirt`,
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 1599,
+},{
+    image: 'images/men-slim-fit-summer-shorts-gray.jpg',
+    name: `Men's Slim-Fit Summer Shorts`,
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 1699,
+},{
+    image: 'images/round-sunglasses-black.jpg',
+    name: `Round Sunglasses`,
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 1560,
+},{
+    image: 'images/sky-flower-stud-earrings.webp',
+    name: `Sterling Silver Sky Flower Stud Earrings`,
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 1799, 
+},{
+    image: 'images/straw-sunhat.webp',
+    name: `Straw Lifeguard Sun Hat`,
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2200,   
+},{
+    image: 'images/women-chiffon-beachwear-coverup-black.jpg',
+    name: `Women's Chiffon Beachwear Cover Up - Black`,
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2070,   
+},{
+    image: 'images/duvet-cover-set-blue-twin.jpg',
+    name: `Duvet Cover Set with Zipper Closure`,
+    rating: {
+        stars: 4.5,
+        
+    },
+    price: 2399,  
+}];
